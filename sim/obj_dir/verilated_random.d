234083013acbfc3e0e9ce44527ade5799a97868f
ghostNotFounded <arsheya.par@gmail.com>
2026-08-25 20:35:59 +0530

    Remove sim/ directory from tracking and add .gitignore

--- sim/obj_dir/verilated_random.d
+++
@@ -1,7 +0,0 @@
-verilated_random.o: /usr/share/verilator/include/verilated_random.cpp \
- /usr/share/verilator/include/verilated_random.h \
- /usr/share/verilator/include/verilated.h \
- /usr/share/verilator/include/verilated_config.h \
- /usr/share/verilator/include/verilatedos.h \
- /usr/share/verilator/include/verilated_types.h \
- /usr/share/verilator/include/verilated_funcs.h
